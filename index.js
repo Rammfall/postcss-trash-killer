2a39c24502d53473120bb9a5929667f643a49fdd
rammfall <15bogdan17@gmail.com>
2020-03-16 16:20:18 +0200

    Some functional

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
+const fs = require('fs');
+const path = require('path');
 const postcss = require('postcss');
 
+const REGEX = /\[.*?\]|[:*][a-z-_:]+|[^a-z-_]+/g;
 const DEFAULT_OPTIONS = {
-  whiteList: ['html', 'body', 'head']
+  path: 'app/',
+  exts: [],
+  whitelist: ['body', 'html', 'slick', 'slick-track', 'aos']
 };
 
-module.exports = postcss.plugin('postcss-trash-killer', opts => {
-  const { whiteList } = [...opts, ...DEFAULT_OPTIONS];
-  return root => {
-    root.walkDecls(decl => {
-      // eslint-disable-next-line no-console
-      console.log(whiteList, decl);
+const getFiles = (pathName, exts) =>
+  fs.readdirSync(pathName).flatMap(file => {
+    var filename = path.join(pathName, file);
+    if (
+      fs.lstatSync(filename).isDirectory() &&
+      !filename.includes('node_modules')
+    ) {
+      return getFiles(filename, exts);
+    } else if (exts.includes(path.extname(filename))) {
+      return [filename];
+    } else {
+      return [];
+    }
+  });
+
+const getContent = files => files.map(f => fs.readFileSync(f, 'utf8')).join();
+const getWords = content =>
+  content
+    .toLowerCase()
+    .split(REGEX)
+    .filter(Boolean);
+
+module.exports = postcss.plugin('remove-unused', opts => css => {
+  const { path, exts, whitelist } = Object.assign(DEFAULT_OPTIONS, opts);
+  const content = getWords(getContent(getFiles(path, exts))).concat(whitelist);
+
+  css.walkRules(rule => {
+    rule.selectors = rule.selectors.filter(selector => {
+      const res = getWords(selector).every(word => {
+        return content.includes(word);
+      });
+
+      if (!res) {
+        rule.remove();
+        return false;
+      }
+      return true;
     });
-  };
+  });
 });
--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,5 +1,12 @@
-describe('First test', () => {
-  test('Must be true', () => {
-    expect(true).toEqual(true);
-  });
+const postcss = require('postcss');
+
+const plugin = require('../index');
+
+function run(input, output, options) {
+  options = { path: 'test/', exts: ['.html'] };
+  expect(postcss([plugin(options)]).process(input).css).toBe(output);
+}
+
+it('Must remove useless selector with rule', () => {
+  run('.test {color: red;} .notest {color: black;}', '.test {color: red;}');
 });
